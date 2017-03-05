e620d8e092eb2d5a2920f37ad349422cde8dea9a
andres <ajarana@email.wm.edu>
2017-03-05 16:30:46 -0500

    rt

--- js/agame.js
+++ js/agame.js
@@ -896,7 +896,7 @@ function setDimensions() {
         if (smallTime == 0) {
           reactionTimeFeedback.innerHTML = "";
 
-          infectionOrigins();
+          // infectionOrigins();
         } else {
           smallScreenCountdown();
         }
